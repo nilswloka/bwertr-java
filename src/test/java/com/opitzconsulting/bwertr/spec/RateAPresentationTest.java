308763fd965cf06669bdf98a8a43008f31936bf4
Stefan Scheidt <stefan.scheidt@opitz-consulting.com>
2011-05-23 11:21:40 +0200

    Refactoring after first iteration.

--- src/test/java/com/opitzconsulting/bwertr/spec/RateAPresentationTest.java
+++ src/test/java/com/opitzconsulting/bwertr/spec/RateAPresentationTest.java
@@ -1,23 +1,10 @@
 package com.opitzconsulting.bwertr.spec;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-
 public class RateAPresentationTest extends AbstractBwertrSpec {
 
     public String ratingShownAfterRatingWith(String givenRating) {
-        // Rate with given rating
-        // * Visit bwertr
-        WebDriver webDriver = new HtmlUnitDriver();
-        webDriver.get("http://localhost:8080/");
-        // * Select option and rate
-        WebElement option = webDriver.findElement(By.id(givenRating));
-        option.setSelected();
-        option.submit();
-        // Return rating shown
-        return webDriver.findElement(By.id("submittedRating")).getText();
+        bwertrDriver.rateWith(givenRating);
+        return bwertrDriver.getThankYouPage().ratingShown();
     }
 
 }
---
+++ src/test/java/com/opitzconsulting/bwertr/spec/ThankYouPage.java
@@ -0,0 +1,17 @@
+package com.opitzconsulting.bwertr.spec;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class ThankYouPage {
+
+    private WebDriver webDriver;
+
+    public ThankYouPage(WebDriver webDriver) {
+        this.webDriver = webDriver;
+    }
+
+    public String ratingShown() {
+        return webDriver.findElement(By.id("submittedRating")).getText();
+    }
+}
