70af6a7f70f6d656b918384ab52d41570823dd91
Stefan Scheidt <stefan.scheidt@opitz-consulting.com>
2011-05-26 11:29:00 +0200

    Refactoring of BwertrController and Tests. Introduce Driver and Page Objects.

--- src/test/java/com/opitzconsulting/bwertr/spec/ShowNumberOfRatingsTest.java
+++ src/test/java/com/opitzconsulting/bwertr/spec/ShowNumberOfRatingsTest.java
@@ -1,30 +1,23 @@
 package com.opitzconsulting.bwertr.spec;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
-import org.springframework.test.jdbc.SimpleJdbcTestUtils;
-
 public class ShowNumberOfRatingsTest extends AbstractSpringBwertrSpec {
 
-    @Autowired
-    private SimpleJdbcTemplate simpleJdbcTemplate;
-
     public String numberOfRatingsShownWhenThereAre(int numberOfRatings) {
-        // Ensure number of ratings exist
-        // * Reset application
-        SimpleJdbcTestUtils.deleteFromTables(simpleJdbcTemplate, "RATINGS");
-        // * Create number of ratings
-        for (int count = 0; count < numberOfRatings; count++) {
-            simpleJdbcTemplate.update("INSERT INTO RATINGS (RATING) VALUES (?)", 0);
-        }
-        // Visit application
-        WebDriver webDriver = new HtmlUnitDriver();
-        webDriver.get("http://localhost:8080/");
-        // Return number of ratings shown
-        return webDriver.findElement(By.id("numberOfRatings")).getText();
+        ensureNumberOfRatingsExist(numberOfRatings);
+        bwertrDriver.visitBwertr();
+        return bwertrDriver.getWelcomePage().numberOfRatingsShown();
     }
+
+    private void ensureNumberOfRatingsExist(int numberOfRatings) {
+        resetApplication();
+        createRatings(numberOfRatings);
+    }
+
+    private void createRatings(int numberOfRatings) {
+        for (int count = 0; count < numberOfRatings; count++) {
+            bwertrDriver.rateWith("Poor");
+        }
+    }
+
 
 }
