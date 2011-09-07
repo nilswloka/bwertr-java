3377dea94c8582fa6d3acf3437d79d8aaf0a8030
Nils Wloka <mail@nilswloka.com>
2011-09-07 12:08:12 +0200

    Fixed AbstractSpringBwertrSpec to use JdbcTemplate instead of SimpleJdbcTemplate

--- src/test/java/com/opitzconsulting/bwertr/spec/AbstractSpringBwertrSpec.java
+++ src/test/java/com/opitzconsulting/bwertr/spec/AbstractSpringBwertrSpec.java
@@ -2,18 +2,18 @@ package com.opitzconsulting.bwertr.spec;
 
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.jdbc.SimpleJdbcTestUtils;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath*:/META-INF/spring/applicationContext*.xml")
 public abstract class AbstractSpringBwertrSpec extends AbstractBwertrSpec {
+
     @Autowired
-    public SimpleJdbcTemplate simpleJdbcTemplate;
+    private JdbcTemplate jdbcTemplate;
 
     protected void resetApplication() {
-        SimpleJdbcTestUtils.deleteFromTables(simpleJdbcTemplate, "RATINGS");
+        jdbcTemplate.update("DELETE FROM RATINGS");
     }
 }
